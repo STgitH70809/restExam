eabf04dd414af1664f825d006524c202a02320e3
STgitH70809 <sontra70809@gmail.com>
2018-12-07 09:35:09 +0700

    modified:   pom.xml 	modified:   src/main/java/personService/mainApp.java 	modified:   .idea/compiler.xml 	modified:   .idea/httpRequests/http-requests-log.http 	modified:   .idea/workspace.xml

--- src/main/java/personService/mainApp.java
+++ src/main/java/personService/mainApp.java
@@ -2,8 +2,10 @@ package personService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class mainApp {
     public static void main(String[] args) {
         SpringApplication.run(mainApp.class, args);
